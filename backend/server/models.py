cca95090e68ca0380170bb30d16b3876a07df427
Abhinav Geethan <72859534+abhinavgeethan@users.noreply.github.com>
2021-07-26 19:35:30 +0400

    Updating password size #2

--- backend/server/models.py
+++ backend/server/models.py
@@ -5,7 +5,7 @@ from . import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True,nullable=False) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True,nullable=False)
-    password = db.Column(db.String(255),nullable=False)
+    password = db.Column(db.String(1000),nullable=False)
     name = db.Column(db.String(1000),nullable=False)
     username=db.Column(db.String(20),unique=True,nullable=False)
     roles=db.Column(db.Text)
